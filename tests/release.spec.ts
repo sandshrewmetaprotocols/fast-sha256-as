d57757e7570f22aa006ce6d728db7f33ecaf73b4
sandshrewmetadev <inquiries@sandshrew.io>
2024-05-28 18:30:39 +0000

    update

--- tests/release.spec.ts
+++ tests/release.spec.ts
@@ -2,8 +2,11 @@ import { IndexerProgram } from "metashrew-test";
 import path from "path";
 import fs from "fs-extra";
 import { EventEmitter } from "events";
+import { hash } from "fast-sha256-js";
 
 const WASM_BINARY = fs.readFileSync(path.join(__dirname, '..', 'build', 'debug.wasm'));
+
+const tx = new Uint8Array(Array.from(Buffer.from('010000000174a016bad8927ee739aff7289f5574b8f57fcc32272829ac3aef3edb9959ad580000000048473044022018d1fc212affb41dd322fdcdc8d0474d7eab7ab414a712361f81f93df7fd9bed02200d7d431ac3c7b61a46b30f911797fde4ede3014434cb00149ab71eed26335f0201ffffffff0200111024010000001976a91420420e56079150b50fb0617dce4c374bd61eccea88ac0ae1f505000000001976a91410f958cbe9cf6d26c2e2ace39766c2cdb87179f788ac00000000', 'hex')));
 
 
 describe("metashrew index", () => {
@@ -15,6 +18,7 @@ describe("metashrew index", () => {
     indexer.setBlockHeight(0);
     const logPromise = new Promise((resolve) => indexer.on("log", (v) => resolve(v)));
     const result = await indexer.run('test_sha256');
+    console.log('0x' + Buffer.from(hash(tx)).toString('hex'));
     console.log(await logPromise);
   });
 });
